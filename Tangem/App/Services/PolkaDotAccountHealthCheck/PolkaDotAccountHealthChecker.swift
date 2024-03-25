c87a1133df4ca73636a59049c205b0a6d166bce6
Service Account <service@tangem.com>
2024-03-25 04:44:20 +0300

    IOS-6263: Add account health checker (WIP 2/x)
    Signed-off-by: Andrey Fedorov <afedorov@tangem.com>

--- Tangem/App/Services/PolkaDotAccountHealthCheck/PolkaDotAccountHealthChecker.swift
+++ Tangem/App/Services/PolkaDotAccountHealthCheck/PolkaDotAccountHealthChecker.swift
@@ -8,25 +8,43 @@
 
 import Foundation
 import Moya
+import BackgroundTasks
 
 final class PolkaDotAccountHealthChecker {
     private let provider = TangemProvider<SubscanAPITarget>() // TODO: Andrey Fedorov - Move into separate entity like network provider
+    private let encoder: JSONEncoder // TODO: Andrey Fedorov - Move into separate entity like network provider
     private let decoder: JSONDecoder // TODO: Andrey Fedorov - Move into separate entity like network provider
 
+    private var currentlyAnalyzingAccounts: Set<String> = []
+
     // TODO: Andrey Fedorov - Protect access to all storage properties
-    @AppStorageCompat(StorageKeys.analyzedAccounts)
-    private var analyzedAccounts: [String] = []
+    @AppStorageCompat(StorageKeys.fullyAnalyzedAccounts)
+    private var fullyAnalyzedAccounts: [String] = []
 
-    @AppStorageCompat(StorageKeys.analyzedPages)
-    private var analyzedNonceCountMismatches: [String] = []
+    @AppStorageCompat(StorageKeys.analyzedForResetAccounts)
+    private var analyzedForResetAccounts: [String] = []
 
-    @AppStorageCompat(StorageKeys.analyzedPages)
-    private var analyzedPages: [String: [Int]] = [:]
+    @AppStorageCompat(StorageKeys.analyzedForImmortalTransactionsAccounts)
+    private var analyzedForImmortalTransactionsAccounts: [String] = []
+
+    @AppStorageCompat(StorageKeys.lastAnalyzedTransactionIds)
+    private var lastAnalyzedTransactionIds: [String: Int] = [:]
 
     private let isTestnet: Bool
 
+    private var backgroundTaskIdentifier: String {
+        guard let bundleIdentifier: String = InfoDictionaryUtils.bundleIdentifier.value() else {
+            preconditionFailure("Unable to get app bundle identifier")
+        }
+        return bundleIdentifier + "." + Constants.backgroundTaskName
+    }
+
     init(isTestnet: Bool) {
         self.isTestnet = isTestnet
+
+        encoder = JSONEncoder()
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+
         decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
 
@@ -34,21 +52,24 @@ final class PolkaDotAccountHealthChecker {
     }
 
     func analyzeAccountIfNeeded(_ account: String) {
-        if analyzedAccounts.contains(account) {
+        guard !fullyAnalyzedAccounts.contains(account) else {
             return
         }
 
-        runTask(in: self) { try await $0.scheduleNormalHealthCheck(for: account) }
+        currentlyAnalyzingAccounts.insert(account)
+
+        runTask(in: self) { try await $0.scheduleForegroundHealthCheck(for: account) }
     }
+
+    // MARK: - Setup
 
     private func setup() {
         runTask(in: self) { await $0.setupObservers() }
+        registerBackgroundTask()
+        cancelBackgroundHealthCheck() // Cancels all tasks from previous runs
     }
 
     private func setupObservers() async {
-        for await _ in NotificationCenter.default.notifications(named: UIApplication.backgroundRefreshStatusDidChangeNotification) {
-            handleBackgroundRefreshStatusChange()
-        }
         for await _ in NotificationCenter.default.notifications(named: UIApplication.didEnterBackgroundNotification) {
             handleApplicationStatusChange(isBackground: true)
         }
@@ -57,14 +78,64 @@ final class PolkaDotAccountHealthChecker {
         }
     }
 
-    private func scheduleNormalHealthCheck(for account: String) async throws {
-        await checkNonceCountMismatch(for: account)
-        await checkImmortalTransactions(for: account)
+    private func registerBackgroundTask() {
+        let result = BGTaskScheduler.shared.register(forTaskWithIdentifier: backgroundTaskIdentifier, using: nil) { [weak self] task in
+            if let task = task as? BGProcessingTask {
+                self?.handleBackgroundProcessingTask(task)
+            } else {
+                preconditionFailure("Unsupported type of background task '\(type(of: task))' received") // TODO: Andrey Fedorov - Add proper logging
+            }
+        }
+        print(#function, result) // TODO: Andrey Fedorov - Add proper logging
     }
 
-    private func checkNonceCountMismatch(for account: String) async {
-        // TODO: Andrey Fedorov - check if we've alreade have in-flight check of this kind
-        if analyzedNonceCountMismatches.contains(account) {
+    // MARK: - Foreground health check
+
+    private func scheduleForegroundHealthCheck(for account: String) async throws {
+        await withTaskGroup(of: Void.self) { taskGroup in
+            taskGroup.addTask {
+                await self.checkAccountForReset(account)
+            }
+            taskGroup.addTask {
+                await self.checkIfAccountContainsImmortalTransactions(account)
+            }
+        }
+    }
+
+    // MARK: - Background health check
+
+    private func scheduleBackgroundHealthCheck() {
+        let request = BGProcessingTaskRequest(identifier: backgroundTaskIdentifier)
+        request.earliestBeginDate = Date(timeIntervalSinceNow: Constants.backgroundTaskDelay) // Allows already running foreground checks to finish
+        request.requiresNetworkConnectivity = true
+
+        do {
+            try BGTaskScheduler.shared.submit(request)
+        } catch {
+            print(error) // TODO: Andrey Fedorov - Catch & log error properly
+        }
+    }
+
+    private func cancelBackgroundHealthCheck() {
+        BGTaskScheduler.shared.cancel(taskRequestWithIdentifier: backgroundTaskIdentifier)
+    }
+
+    private func handleBackgroundProcessingTask(_ task: BGProcessingTask) {}
+
+    @MainActor
+    private func handleApplicationStatusChange(isBackground: Bool) {
+        if isBackground {
+            scheduleBackgroundHealthCheck()
+        } else {
+            // TODO: Andrey Fedorov - Restart normal check here
+            cancelBackgroundHealthCheck()
+        }
+    }
+
+    // MARK: - Shared logic
+
+    private func checkAccountForReset(_ account: String) async {
+        guard !analyzedForResetAccounts.contains(account) else {
             return
         }
 
@@ -72,40 +143,102 @@ final class PolkaDotAccountHealthChecker {
         // TODO: Andrey Fedorov - Try to map API error first (using common helper perhaps?)
         do {
             let accountInfo = try await provider
-                .asyncRequest(for: .init(isTestnet: isTestnet, target: .getAccountInfo(address: account)))
+                .asyncRequest(
+                    for: .init(
+                        isTestnet: isTestnet,
+                        encoder: encoder,
+                        target: .getAccountInfo(address: account)
+                    )
+                )
                 .filterSuccessfulStatusAndRedirectCodes()
                 .map(SubscanAPIResult.AccountInfo.self, using: decoder)
                 .data
                 .account
 
-            if analyzedNonceCountMismatches.contains(account) {
+            // Double checking is a must since theoretically there can be multiple ongoing checks
+            guard !analyzedForResetAccounts.contains(account) else {
                 return
             }
 
             // `accountInfo.nonce` can be equal to or greater than the count of extrinsics,
             // but can't it be less (unless the account has been reset)
-            let metric: AccountHealthMetric = .hasNonceCountMismatch(value: accountInfo.nonce < accountInfo.countExtrinsic)
+            let metric: AccountHealthMetric = .hasBeenReset(value: accountInfo.nonce < accountInfo.countExtrinsic)
             sendAccountHealthMetric(metric)
-            analyzedNonceCountMismatches.append(account)
+            analyzedForResetAccounts.append(account)
         } catch {
-            // TODO: Andrey Fedorov - Catch error
+            print(error) // TODO: Andrey Fedorov - Catch & log error properly
         }
     }
 
-    private func checkImmortalTransactions(for account: String) async {
-        // TODO: Andrey Fedorov - check if we've alreade have in-flight check of this kind
-    }
+    private func checkIfAccountContainsImmortalTransactions(_ account: String) async {
+        // TODO: Andrey Fedorov - Add retries (using common helper perhaps?)
+        // TODO: Andrey Fedorov - Try to map API error first (using common helper perhaps?)
+        do {
+            var foundImmortalTransaction = false
 
-    private func scheduleBackgroundHealthCheck(for account: String) {}
+            transactionsListLoop: while true {
+                let afterId = lastAnalyzedTransactionIds[account, default: Constants.initialTransactionId]
+                let transactions = try await provider
+                    .asyncRequest(
+                        for: .init(
+                            isTestnet: isTestnet,
+                            encoder: encoder,
+                            target: .getExtrinsicsList(
+                                address: account,
+                                afterId: afterId,
+                                page: Constants.startPage,
+                                limit: Constants.pageSize
+                            )
+                        )
+                    )
+                    .filterSuccessfulStatusAndRedirectCodes()
+                    .map(SubscanAPIResult.ExtrinsicsList.self, using: decoder)
+                    .data
+                    .extrinsics
 
-    @MainActor
-    private func handleBackgroundRefreshStatusChange() {
-        // TODO: Andrey Fedorov - re-schedule bg job if user enabled bg refresh
-    }
+                // Checking if we've reached the end of the transactions list
+                guard let transactions = transactions?.nilIfEmpty else {
+                    break transactionsListLoop
+                }
 
-    @MainActor
-    private func handleApplicationStatusChange(isBackground: Bool) {
-        // TODO: Andrey Fedorov - re-schedule bg job if user enabled bg refresh
+                for transaction in transactions {
+                    // Adding small delays between consecutive fetches of transaction details to avoid hitting the API rate limit
+                    try await _Concurrency.Task.sleep(seconds: Constants.transactionInfoCheckDelay) // TODO: Andrey Fedorov - Add some random jitter
+
+                    let lifetime = try await provider
+                        .asyncRequest(
+                            for: .init(
+                                isTestnet: isTestnet,
+                                encoder: encoder,
+                                target: .getExtrinsicInfo(hash: transaction.extrinsicHash)
+                            )
+                        )
+                        .filterSuccessfulStatusAndRedirectCodes()
+                        .map(SubscanAPIResult.ExtrinsicInfo.self, using: decoder)
+                        .data
+                        .lifetime
+
+                    lastAnalyzedTransactionIds[account] = transaction.id
+
+                    // Early exit if we've found at least one immortal transaction
+                    if lifetime == nil {
+                        foundImmortalTransaction = true
+                        break transactionsListLoop
+                    }
+                }
+            }
+
+            // Double checking is a must since theoretically there can be multiple ongoing checks
+            guard !analyzedForImmortalTransactionsAccounts.contains(account) else {
+                return
+            }
+
+            let metric: AccountHealthMetric = .hasImmortalTransaction(value: foundImmortalTransaction)
+            sendAccountHealthMetric(metric)
+            analyzedForImmortalTransactionsAccounts.append(account)
+        } catch {
+            print(error) // TODO: Andrey Fedorov - Catch & log error properly
+        }
     }
 
     @MainActor
@@ -116,7 +249,7 @@ final class PolkaDotAccountHealthChecker {
 
 private extension PolkaDotAccountHealthChecker {
     enum AccountHealthMetric {
-        case hasNonceCountMismatch(value: Bool)
+        case hasBeenReset(value: Bool)
         case hasImmortalTransaction(value: Bool)
     }
 }
@@ -125,8 +258,22 @@ private extension PolkaDotAccountHealthChecker {
 
 private extension PolkaDotAccountHealthChecker {
     enum StorageKeys: String, RawRepresentable {
-        case analyzedAccounts = "polka_dot_account_health_checker_analyzed_accounts"
-        case analyzedNonceMismatches = "polka_dot_account_health_checker_analyzed_nonce_mismatches"
-        case analyzedPages = "polka_dot_account_health_checker_analyzed_pages"
+        case fullyAnalyzedAccounts = "polka_dot_account_health_checker_fully_analyzed_accounts"
+        case analyzedForResetAccounts = "polka_dot_account_health_checker_analyzed_for_reset_accounts"
+        case analyzedForImmortalTransactionsAccounts = "polka_dot_account_health_checker_analyzed_for_immortal_transactions_accounts"
+        case lastAnalyzedTransactionIds = "polka_dot_account_health_checker_last_analyzed_transaction_ids"
+    }
+}
+
+private extension PolkaDotAccountHealthChecker {
+    enum Constants {
+        /// - Warning: Must match the value specified in the `Info.plist`.
+        static let backgroundTaskName = "PolkaDotAccountHealthCheckTask"
+        /// 10 minutes.
+        static let backgroundTaskDelay = 60.0 * 10.0
+        static let initialTransactionId = 0
+        static let startPage = 0
+        static let transactionInfoCheckDelay = 1.0
+        static let pageSize = 100
     }
 }
