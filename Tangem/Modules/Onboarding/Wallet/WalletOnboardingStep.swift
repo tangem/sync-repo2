aeb444471e1c7ecb11929acbf3670dff2b3e75fb
Service Account <service@tangem.com>
2024-06-28 19:37:38 +0200

    IOS-6136: Add missed localizations
    Signed-off-by: Andrey Fedorov <afedorov@tangem.com>

--- Tangem/Modules/Onboarding/Wallet/WalletOnboardingStep.swift
+++ Tangem/Modules/Onboarding/Wallet/WalletOnboardingStep.swift
@@ -51,12 +51,13 @@ enum WalletOnboardingStep: Equatable {
 
     var navbarTitle: String {
         switch self {
-        case .pushNotifications: return "Push Notifications" // TODO: https://tangem.atlassian.net/browse/IOS-6136
         case .createWallet, .backupIntro: return Localization.onboardingGettingStarted
         case .scanPrimaryCard, .selectBackupCards: return Localization.onboardingNavbarTitleCreatingBackup
         case .backupCards: return Localization.onboardingButtonFinalizeBackup
         case .saveUserWallet: return Localization.onboardingNavbarSaveWallet
         case .success: return Localization.commonDone
+        case .pushNotifications:
+            return Localization.onboardingTitleNotifications
         case .createWalletSelector:
             return Localization.walletTitle
         case .seedPhraseIntro, .seedPhraseGeneration, .seedPhraseUserValidation:
