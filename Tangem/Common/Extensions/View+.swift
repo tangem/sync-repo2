0d058ee84410bc2edb68dbff80a10884731026f2
Service Account <service@tangem.com>
2024-07-10 15:25:22 +0200

    curr state
    Signed-off-by: Andrey Fedorov <afedorov@tangem.com>

--- Tangem/Common/Extensions/View+.swift
+++ Tangem/Common/Extensions/View+.swift
@@ -106,4 +106,69 @@ extension View {
             #endif
         }
     }
+
+    // FIXME: Andrey Fedorov - Test only, remove when not needed
+    @available(*, deprecated, message: "For test purposes only, remove when not needed")
+    @ViewBuilder
+    func printSize(_ prefix: String) -> some View {
+        readGeometry(\.size) { print(prefix, $0) }
+    }
+
+    // FIXME: Andrey Fedorov - Test only, remove when not needed
+    @available(*, deprecated, message: "For test purposes only, remove when not needed")
+    @inline(__always)
+    static func printChanges(
+        file: StaticString = #fileID,
+        line: UInt = #line,
+        column: UInt = #column
+    ) {
+        #if DEBUG
+        if #available(iOS 15.0, *) {
+            Self._printChanges()
+        } else {
+            print("View body computed in \(file):\(line):\(column)")
+        }
+        #endif
+    }
+
+    // FIXME: Andrey Fedorov - Test only, remove when not needed
+    @available(*, deprecated, message: "For test purposes only, remove when not needed")
+    func debugBorder(color: Color, width: CGFloat = 1.0 / UIScreen.main.scale) -> some View {
+        modifier(DebugBorderViewModifier(color: color, width: width))
+    }
+}
+
+// MARK: - Auxiliary types
+
+// FIXME: Andrey Fedorov - Test only, remove when not needed
+private struct DebugBorderViewModifier: ViewModifier {
+    let color: Color
+    let width: CGFloat
+
+    func body(content: Content) -> some View {
+        #if DEBUG
+        let showDebugBorders = UserDefaults.standard.bool(forKey: "com.tangem.ShowDebugBorders")
+
+        return content.border(
+            showDebugBorders ? color : .clear,
+            width: showDebugBorders ? width : 0.0
+        )
+        #else
+        return content
+        #endif
+    }
+}
+
+// MARK: - Convenience extensions
+
+// FIXME: Andrey Fedorov - Test only, remove when not needed
+extension Color {
+    @available(*, deprecated, message: "For test purposes only, remove when not needed")
+    static var random: Self {
+        return Color(
+            hue: .random(in: 0.0 ... 1.0),
+            saturation: 1.0,
+            brightness: 1.0
+        )
+    }
 }
