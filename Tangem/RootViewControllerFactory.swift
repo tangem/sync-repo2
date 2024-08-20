52aa5466104c3eea958c87db42a313c88c6206b0
Service Account <service@tangem.com>
2024-08-20 14:15:47 +0200

    IOS-7364: Update Jira refs in TODOs
    Signed-off-by: Andrey Fedorov <afedorov@tangem.com>

--- Tangem/RootViewControllerFactory.swift
+++ Tangem/RootViewControllerFactory.swift
@@ -26,7 +26,7 @@ struct RootViewControllerFactory {
 
         let contentViewController = UIHostingController(rootView: rootView)
 
-        // TODO: Andrey Fedorov - Adjust all numeric values here for different devices and safe area (IOS-7364)
+        // TODO: Andrey Fedorov - Adjust all numeric values here for different devices and safe area (IOS-7664)
         let containerViewController = OverlayContentContainerViewController(
             contentViewController: contentViewController,
             overlayCollapsedHeight: 102.0, // https://www.figma.com/design/91bpyCrISuWSvUzTLmcYRc/iOS-%E2%80%93-Draft?node-id=21140-91435&t=Z1kPdSQJ0JLoYgW0-4
--- Tangem/UIComponents/OverlayContentContainer/UIKit/OverlayContentContainerViewController.swift
+++ Tangem/UIComponents/OverlayContentContainer/UIKit/OverlayContentContainerViewController.swift
@@ -203,7 +203,7 @@ final class OverlayContentContainerViewController: UIViewController {
 
     /// - Note: The order in which this method is called matters. Must be called between `setupContent` and `setupOverlay`.
     private func setupBackgroundShadowView() {
-        // TODO: Andrey Fedorov - Add support for dark mode (adjust content view contrast instead of using background shadow) (IOS-7364)
+        // TODO: Andrey Fedorov - Add support for dark mode (adjust content view contrast or background colors instead of using background shadow) (IOS-7664)
         backgroundShadowView.backgroundColor = .black
         backgroundShadowView.alpha = Constants.minBackgroundShadowViewAlpha
         backgroundShadowView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
@@ -478,7 +478,7 @@ final class OverlayContentContainerViewController: UIViewController {
             min: overlayExpandedVerticalOffset,
             max: overlayCollapsedVerticalOffset
         )
-        overlayViewTopAnchorConstraint?.constant = newVerticalOffset // TODO: Andrey Fedorov - Add rubber-banding (IOS-7364)
+        overlayViewTopAnchorConstraint?.constant = newVerticalOffset // TODO: Andrey Fedorov - Add rubber-banding (IOS-7664)
         gestureRecognizer.setTranslation(.zero, in: nil)
 
         updateProgress(verticalOffset: newVerticalOffset, animationContext: nil)
