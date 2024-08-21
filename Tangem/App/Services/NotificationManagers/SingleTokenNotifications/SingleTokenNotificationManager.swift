32685b034ba2d2b66ab64473f13c409802b9d71d
Service Account <service@tangem.com>
2024-08-21 12:27:57 +0200

    IOS-7666 Add todo

--- Tangem/App/Services/NotificationManagers/SingleTokenNotifications/SingleTokenNotificationManager.swift
+++ Tangem/App/Services/NotificationManagers/SingleTokenNotifications/SingleTokenNotificationManager.swift
@@ -95,6 +95,7 @@ final class SingleTokenNotificationManager {
 
         /// We can't use `Blockchain.polygon(testnet: false).currencySymbol` here
         /// because it will be changed after some time to `"POL"`
+        // TODO: https://tangem.atlassian.net/browse/IOS-7695
         if walletModel.tokenItem.currencySymbol == CurrencySymbol.matic {
             events.append(.maticMigration)
         }
