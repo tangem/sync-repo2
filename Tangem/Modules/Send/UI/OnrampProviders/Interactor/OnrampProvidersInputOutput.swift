7e20b79b5a4147120eb2cd0e321a59c4e2f74137
Service Account <service@tangem.com>
2024-11-11 12:36:51 +0000

    IOS-8360 [Onramp] Add possibility to work with  Providers and PaymentMethod via IO (#4146)

---
+++ Tangem/Modules/Send/UI/OnrampProviders/Interactor/OnrampProvidersInputOutput.swift
@@ -0,0 +1,21 @@
+//
+//  OnrampProvidersInputOutput.swift
+//  Tangem
+//
+//  Created by Sergey Balashov on 31.10.2024.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Combine
+import TangemExpress
+
+protocol OnrampProvidersInput: AnyObject {
+    var selectedOnrampProvider: OnrampProvider? { get }
+    var selectedOnrampProviderPublisher: AnyPublisher<LoadingValue<OnrampProvider>?, Never> { get }
+
+    var onrampProvidersPublisher: AnyPublisher<[OnrampProvider], Never> { get }
+}
+
+protocol OnrampProvidersOutput: AnyObject {
+    func userDidSelect(provider: OnrampProvider)
+}
--- Tangem/Modules/Send/UI/OnrampProviders/OnrampProvidersCompactView/OnrampProvidersCompactProviderView/OnrampProvidersCompactProviderViewData.swift
+++ Tangem/Modules/Send/UI/OnrampProviders/OnrampProvidersCompactView/OnrampProvidersCompactProviderView/OnrampProvidersCompactProviderViewData.swift
@@ -1,14 +1,14 @@
 //
-//  OnrampProvidersCompactViewModel.swift
+//  OnrampProvidersCompactProviderViewData.swift
 //  TangemApp
 //
-//  Created by Sergey Balashov on 25.10.2024.
+//  Created by Sergey Balashov on 31.10.2024.
 //  Copyright © 2024 Tangem AG. All rights reserved.
 //
 
 import Foundation
 
-struct OnrampProvidersCompactViewData {
+struct OnrampProvidersCompactProviderViewData {
     let iconURL: URL?
     let paymentMethodName: String
     let providerName: String
@@ -16,10 +16,10 @@ struct OnrampProvidersCompactViewData {
     let action: () -> Void
 }
 
-extension OnrampProvidersCompactViewData: Hashable, Identifiable {
+extension OnrampProvidersCompactProviderViewData: Hashable, Identifiable {
     var id: Int { hashValue }
 
-    static func == (lhs: OnrampProvidersCompactViewData, rhs: OnrampProvidersCompactViewData) -> Bool {
+    static func == (lhs: Self, rhs: Self) -> Bool {
         lhs.hashValue == rhs.hashValue
     }
 
@@ -31,7 +31,7 @@ extension OnrampProvidersCompactViewData: Hashable, Identifiable {
     }
 }
 
-extension OnrampProvidersCompactViewData {
+extension OnrampProvidersCompactProviderViewData {
     enum Badge: Hashable {
         case bestRate
     }
