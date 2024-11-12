151e770e8230ff5a2e95c788a7ac607c11f2bf55
Service Account <service@tangem.com>
2024-11-12 11:52:48 +0000

    IOS-8310 [Onramp] Updating quotes logic (#4164)

--- TangemExpress/Models/Onramp/OnrampProvider.swift
+++ TangemExpress/Models/Onramp/OnrampProvider.swift
@@ -6,7 +6,18 @@
 //  Copyright © 2024 Tangem AG. All rights reserved.
 //
 
-public struct OnrampProvider: Hashable {
-    // TODO: https://tangem.atlassian.net/browse/IOS-8310
-    public let provider: OnrampPair.Provider
+public class OnrampProvider {
+    public let provider: ExpressProvider
+    public let paymentMethod: OnrampPaymentMethod
+    public let manager: OnrampProviderManager
+
+    init(
+        provider: ExpressProvider,
+        paymentMethod: OnrampPaymentMethod,
+        manager: OnrampProviderManager
+    ) {
+        self.provider = provider
+        self.paymentMethod = paymentMethod
+        self.manager = manager
+    }
 }
