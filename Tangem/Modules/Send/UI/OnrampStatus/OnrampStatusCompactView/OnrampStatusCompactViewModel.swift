b4e9a5be6708d0affa0ca56b2d7d46d963a2be59
Service Account <service@tangem.com>
2024-12-02 13:24:01 +0000

    IOS-8635 [Onramp] Fix finish view (#4309)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Tangem Service <gitservice@tangem.com>

---
+++ Tangem/Modules/Send/UI/OnrampStatus/OnrampStatusCompactView/OnrampStatusCompactViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  OnrampStatusCompactViewModel.swift
+//  TangemApp
+//
+//  Created by Sergey Balashov on 28.11.2024.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Combine
+import Foundation
+
+class OnrampStatusCompactViewModel: ObservableObject {
+    init() {}
+}
