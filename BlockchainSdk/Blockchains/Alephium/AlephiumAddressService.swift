61d3d3ae6943f9bf47d6e60158a61e8c57a9e141
Service Account <service@tangem.com>
2025-01-21 19:50:49 +0300

    IOS-8982 Alephium address service impl (#4498)

--- BlockchainSdk/Blockchains/Alephium/AlephiumAddressService.swift
+++ BlockchainSdk/Blockchains/Alephium/AlephiumAddressService.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import TangemSdk
 
 struct AlephiumAddressService {}
 
@@ -14,12 +15,33 @@ struct AlephiumAddressService {}
 
 extension AlephiumAddressService: AddressProvider, AddressValidator {
     func makeAddress(for publicKey: Wallet.PublicKey, with addressType: AddressType) throws -> any Address {
-        // TODO: - https://tangem.atlassian.net/browse/IOS-8982
-        throw WalletError.empty
+        let compressedKey = try Secp256k1Key(with: publicKey.blockchainKey).compress()
+
+        guard let blake2bHash = compressedKey.hashBlake2b(outputLength: 32) else {
+            throw Error.undefinedBlake2bHash
+        }
+
+        let hashData = Data(hexString: Constants.prefixAddressValue.addHexPrefix()) + blake2bHash
+        let address = hashData.base58EncodedString
+
+        return PlainAddress(value: address, publicKey: publicKey, type: addressType)
     }
 
     func validate(_ address: String) -> Bool {
-        // TODO: - https://tangem.atlassian.net/browse/IOS-8982
-        false
+        let hexDataString = address.base58DecodedData.hexString
+        let withoutHexString = hexDataString.removeHexPrefix()
+        return withoutHexString.hasPrefix(Constants.prefixAddressValue) && withoutHexString.count == 66
+    }
+}
+
+// MARK: - Helpers
+
+extension AlephiumAddressService {
+    enum Constants {
+        static let prefixAddressValue = "00"
+    }
+
+    enum Error: LocalizedError {
+        case undefinedBlake2bHash
     }
 }
