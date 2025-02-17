4c30c4ed743718c029b32dfd4f05c9707de438b5
Service Account <service@tangem.com>
2025-02-17 13:49:52 +0300

    IOS-9004 Local TOS (#60)
    Co-authored-by: Mike <mandreev@tangen.com>

---
+++ Tangem/Modules/WebViewContainer/WebViewTimeoutSettings.swift
@@ -0,0 +1,14 @@
+//
+//  TimeoutSettings.swift
+//  TangemApp
+//
+//  Created by Mikhail Andreev on 2/14/25.
+//  Copyright © 2025 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+struct WebViewTimeoutSettings {
+    let interval: TimeInterval
+    let fallbackURL: URL?
+}
