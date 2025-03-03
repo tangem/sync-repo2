11157ad9784bbe74d319b62b0523f5fe7727be80
Service Account <service@tangem.com>
2025-03-03 18:45:07 +0100

    IOS-9242: Add NFT data provider (WIP)
    Signed-off-by: Andrey Fedorov <afedorov@tangem.com>

---
+++ Tangem/App/Services/NFT/DataSource/NFTDataProvider.swift
@@ -0,0 +1,45 @@
+//
+//  NFTDataProvider.swift
+//  Tangem
+//
+//  Created by m3g0byt3 on 03.03.2025.
+//  Copyright © 2025 Tangem AG. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+// TODO: Andrey Fedorov - Improve naming and public interface if needed
+final class NFTDataProvider {
+    @Injected(\.userWalletRepository) private var userWalletRepository: UserWalletRepository
+    @Injected(\.nftAvailabilityProvider) private var nftAvailabilityProvider: NFTAvailabilityProvider
+
+    private var bag: Set<AnyCancellable> = []
+
+    init() {
+        bind()
+    }
+
+    // MARK: - Private functions
+
+    private func bind() {
+        userWalletRepository.eventProvider
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: weakify(self, forFunction: NFTDataProvider.handleUserWalletRepositoryEvent))
+            .store(in: &bag)
+    }
+
+    private func handleUserWalletRepositoryEvent(_ event: UserWalletRepositoryEvent) {
+        switch event {
+        case .deleted(let userWalletIds):
+            for userWalletId in userWalletIds {
+                guard let userWallet = userWalletRepository.models.first(where: { $0.userWalletId == userWalletId }) else {
+                    return
+                }
+                nftAvailabilityProvider.setNFTEnabled(false, for: userWallet)
+            }
+        default:
+            break
+        }
+    }
+}
