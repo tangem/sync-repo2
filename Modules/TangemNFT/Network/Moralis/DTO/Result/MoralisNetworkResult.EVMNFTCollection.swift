1a2ad6b1d95cf1949ced8b8d562009dccd8068e8
Service Account <service@tangem.com>
2025-03-06 18:34:01 +0100

    IOS-9236: Use a generic response for some endpoints
    Signed-off-by: Andrey Fedorov <afedorov@tangem.com>

---
+++ Modules/TangemNFT/Network/Moralis/DTO/Result/MoralisNetworkResult.EVMNFTCollection.swift
@@ -0,0 +1,26 @@
+//
+//  MoralisNetworkResult.EVMNFTCollection.swift
+//  TangemNFT
+//
+//  Created by Andrey Fedorov on 03.03.2025.
+//  Copyright © 2025 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+extension MoralisNetworkResult {
+    struct EVMNFTCollection: Decodable {
+        let tokenAddress: String?
+        let possibleSpam: Bool?
+        let contractType: String?
+        let name: String?
+        let symbol: String?
+        let verifiedCollection: Bool?
+        let collectionLogo: String?
+        let collectionBannerImage: String?
+        let floorPrice: String?
+        let floorPriceUSD: String?
+        let floorPriceCurrency: String?
+        let count: Int?
+    }
+}
