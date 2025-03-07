6a0fba9f30367d68b845e0f63b359249bc2eafd4
Service Account <service@tangem.com>
2025-03-07 11:02:48 +0100

    IOS-9236: Fix encoding issues with mixed `camelCase` and `snake_case`-encoded fields in params
    Signed-off-by: Andrey Fedorov <afedorov@tangem.com>

---
+++ Modules/TangemNFT/Network/Moralis/DTO/Params/MoralisNetworkParams.NFTAssetsQuery.swift
@@ -0,0 +1,15 @@
+//
+//  MoralisNetworkParams.NFTAssetsQuery.swift
+//  TangemNFT
+//
+//  Created by Andrey Fedorov on 06.03.2025.
+//  Copyright © 2025 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+extension MoralisNetworkParams {
+    struct NFTAssetsQuery: Encodable {
+        let chain: NFTChain
+    }
+}
