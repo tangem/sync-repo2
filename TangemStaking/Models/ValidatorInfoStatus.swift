31b897c7c70aea013d01fbd59f01cf5ef315258d
Service Account <service@tangem.com>
2025-03-07 13:45:11 +0400

    IOS-9288 Add account activation alert texts

---
+++ TangemStaking/Models/ValidatorInfoStatus.swift
@@ -0,0 +1,15 @@
+//
+//  ValidatorInfoStatus.swift
+//  TangemApp
+//
+//  Created by Dmitry Fedorov on 07.03.2025.
+//  Copyright © 2025 Tangem AG. All rights reserved.
+//
+
+public enum ValidatorInfoStatus {
+    case active
+    case jailed
+    case deactivating
+    case inactive
+    case full
+}
